141fb540c42f3bc2c3cc554ab8d2e37c06c8d87e
Elijah Sylejman Limaj <elijahslimaj@Elijahs-MacBook-Pro.local>
2023-12-07 10:03:31 +0100

    Set up side nav for desktop and mobile

--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -1,3 +1,4 @@
+import Navbar from "./_components/navbar";
 import Sidebar from "./_components/sidebar";
 
 const DashboardLayout = ({ children }: {
@@ -5,7 +6,11 @@ const DashboardLayout = ({ children }: {
 }) => {
     return (
         <div className="h-full">
-            {/* Side bar */}
+            {/* Navbar */}
+            <div className="h-[80px] md:pl-56 fixed inset-y-0 w-full z-50">
+                <Navbar />
+            </div>
+            {/* Sidebar */}
             <div className="hidden md:flex h-full w-56 flex-col fixed inset-y-0 z-50">
                 <Sidebar />
             </div>

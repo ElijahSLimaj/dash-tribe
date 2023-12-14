0effab416505e562b597e07b689d55fe50c2c8a5
Elijah Sylejman Limaj <elijahslimaj@Elijahs-MacBook-Pro.local>
2023-12-14 10:26:51 +0100

    set up edit title form

--- app/(dashboard)/(routes)/teacher/courses/[courseId]/page.tsx
+++ app/(dashboard)/(routes)/teacher/courses/[courseId]/page.tsx
@@ -3,6 +3,7 @@ import { db } from "@/lib/db";
 import { auth } from "@clerk/nextjs";
 import { LayoutDashboard } from "lucide-react";
 import { redirect } from "next/navigation";
+import { TitleForm } from "./_components/title-form";
 
 const CourseIdPage = async ({ params }: {
     params: { courseId: string }
@@ -52,12 +53,16 @@ const CourseIdPage = async ({ params }: {
                 <div>
                     <div className="flex items-center gap-x-2">
                         <IconBadge
-                            icon={LayoutDashboard}       
+                            icon={LayoutDashboard}
                         />
                         <h2 className="text-xl">
                             Customize your course
                         </h2>
                     </div>
+                    <TitleForm 
+                        initialData={course}
+                        courseId={course.id}
+                    />
                 </div>
             </div>
         </div>

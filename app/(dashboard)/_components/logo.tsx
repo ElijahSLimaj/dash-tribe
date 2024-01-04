51673be48ff944ff1fb4dfc507eecdcc167441b2
Elijah Sylejman Limaj <elijahslimaj@Elijahs-MacBook-Pro.local>
2024-01-04 12:33:53 +0100

    modify con

--- app/(dashboard)/_components/logo.tsx
+++ app/(dashboard)/_components/logo.tsx
@@ -2,12 +2,22 @@ import Image from "next/image";
 
 const Logo = () => {
     return (
-        <Image
-            height={130} 
-            width={130} 
-            alt="Logo" 
-            src={"/logo.png"}
-        />
+        <div style={{
+            borderRadius: '50%', // Makes it round
+            width: '80px', // Diameter of the circle
+            height: '80px', // Diameter of the circle
+            overflow: 'hidden', // Ensures the image doesn't exceed the circular boundary
+            display: 'flex', // Ensures the image is centered in the div
+            justifyContent: 'center',
+            alignItems: 'center'
+        }}>
+            <Image
+                height={80} // Set height to 200 for a diameter of 200
+                width={80} // Set width to 200 for a diameter of 200
+                alt="Logo"
+                src={"/logo.svg"}
+            />
+        </div>
     )
 }
 
